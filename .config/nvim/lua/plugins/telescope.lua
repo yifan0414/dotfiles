7f43596f81324301cb249f7a1caecf2c7bfba0ab
SnakeHit <1679634762@qq.com>
2024-01-20 21:19:52 +0800

    nvim backup

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
           ["<Esc>"] = require("telescope.actions").close,
           ["<Tab>"] = require("telescope.actions").move_selection_previous,
           ["<S-Tab>"] = require("telescope.actions").move_selection_next,
-          -- ["<CR>"] = require("telescope.actions").select_vertical,
+          ["<C-Q>"] = require("telescope.actions").select_vertical,
         },
       },
       -- The below pattern is lua regex and not wildcard
@@ -17,6 +17,7 @@ return {
       -- prompt_prefix = "🔍 ",
       prompt_prefix = "🔭 ",
       path_display = { truncate = 6 },
+      layout_config = {height = 0.9, width = 0.9, preview_width = 0.5},
     },
   },
 }
