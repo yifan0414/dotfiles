d5d790c1824f9025f21a88f3815bcef3898619c7
yifan0414 <1679634762@qq.com>
2024-02-03 15:36:10 +0800

    ✨NVIM: update telescope, decrease speed time

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,86 +1,44 @@
--- {"命令名称", "执行的命令"},
-local tmux_command = {
-  { "sent current line to tmux 2 pane", "silent .w !awk '{$1=$1;print}' | xargs -0ri tmux send -t2 {}" },
-  {
-    "sent current line to popup",
-    "silent .w !awk '{$1=$1;print}' | xargs -0ri tmux send -t popup:1 {}; tmux display-popup -d '\\#{pane_current_path}' -xC -yC -w 90\\% -h 85\\% -E 'tmux attach-session -t popup || tmux new-session -s popup'",
-  },
-}
-
-local frequent_command = {
-  { "Show File Path", "call setqflist([{'text': execute('echomsg expand(\"%:p\")')}]) | copen" },
-  {
-    "Open Man In Floaterm",
-    "FloatermNew --width=0.8 --height=0.8 man -k . | fzf | awk '{print $1}' | xargs man | less",
-  },
-  {
-    "输出一个msg到quickfix",
-    "execute 'call setqflist([{\"text\": \"' . input('Enter message: ') . '\"}], \"a\") | copen'",
-  },
-  {
-    "Tokei(count code)",
-    "AsyncRun -mode=term -pos=floaterm -width=0.8 -height=0.8 tokei <root>",
-  },
-}
-
-local yadm_command = {
-  {
-    "Yadm log",
-    -- "call asyncrun#run('', {'scroll':'0', 'post':'copen'}, 'cd ~ && yadm log')" ,
-    "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm log",
-  },
-  {
-    "Yadm nvim",
-    "call asyncrun#run('', {'cwd':'~'},'yadm add .config/nvim && yadm commit -m \"'.input('Enter commit message: ').'\" && yadm push')",
-  },
-  {
-    "Yadm diff",
-    "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm diff",
-  },
-  {
-    "Yadm status",
-    "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm status",
-  },
-  {
-    "Yadm diff this file",
-    "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm diff $(VIM_FILEPATH)",
-  },
-}
-
-local git_command = {
-  {
-    "查看git仓库代码修改行数",
-    "AsyncRun -cwd=<root> -mode=term -pos=floaterm -width=0.8 -height=0.8 git diff --stat HEAD",
-  },
-  {
-    "Git This Repository",
-    "call asyncrun#run('', {'cwd':'<root>'},'git add . && git commit -m \"'.input('Enter commit message: ').'\" && git push')",
-  },
-}
-
 return {
   "nvim-telescope/telescope.nvim",
-  lazy = true,
-  opts = {
+  -- lazy = true,
+  opts = function()
     -- see :help telescope.setup()
-    defaults = {
-      -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/actions/init.lua
-      mappings = {
-        i = {
-          ["<Esc>"] = require("telescope.actions").close,
-          ["<Tab>"] = require("telescope.actions").move_selection_previous,
-          ["<S-Tab>"] = require("telescope.actions").move_selection_next,
-          ["<C-Q>"] = require("telescope.actions").select_vertical,
+    local actions = require("telescope.actions")
+    return {
+      defaults = {
+        -- https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/actions/init.lua
+        mappings = {
+          i = {
+            ["<Esc>"] = actions.close,
+            ["<Tab>"] = actions.move_selection_previous,
+            ["<S-Tab>"] = actions.move_selection_next,
+            ["<C-Q>"] = actions.select_vertical,
+          },
         },
+        -- The below pattern is lua regex and not wildcard
+        -- file_ignore_patterns = { "%.out", "%.pdf", "%.png", "%.ok" },
+        -- prompt_prefix = "🔍 ",
+        prompt_prefix = "🔭 ",
+        path_display = { truncate = 6 },
+        -- layout_config = {height = 0.9, width = 0.9, preview_width = 0.5},
       },
-      -- The below pattern is lua regex and not wildcard
-      -- file_ignore_patterns = { "%.out", "%.pdf", "%.png", "%.ok" },
-      -- prompt_prefix = "🔍 ",
-      prompt_prefix = "🔭 ",
-      path_display = { truncate = 6 },
-      -- layout_config = {height = 0.9, width = 0.9, preview_width = 0.5},
-    },
-  },
+    }
+  end,
+  -- config = function()
+  --   local actions = require("telescope.actions")
+  --   require("telescope").setup({
+  --     defaults = {
+  --       mappings = {
+  --         i = {
+  --           ["<Esc>"] = actions.close,
+  --           ["<Tab>"] = actions.move_selection_previous,
+  --           ["<S-Tab>"] = actions.move_selection_next,
+  --           ["<C-Q>"] = actions.select_vertical,
+  --         },
+  --       },
+  --     },
+  --   })
+  -- end,
   keys = {
     {
       "<Leader>fs",
@@ -92,6 +50,13 @@ return {
     {
       "<Leader>tt",
       function()
+        local tmux_command = {
+          { "sent current line to tmux 2 pane", "silent .w !awk '{$1=$1;print}' | xargs -0ri tmux send -t2 {}" },
+          {
+            "sent current line to popup",
+            "silent .w !awk '{$1=$1;print}' | xargs -0ri tmux send -t popup:1 {}; tmux display-popup -d '\\#{pane_current_path}' -xC -yC -w 90\\% -h 85\\% -E 'tmux attach-session -t popup || tmux new-session -s popup'",
+          },
+        }
         local picker = require("plugins.util.picker") -- lazy load
         picker.telescope_command_picker(tmux_command)
       end,
@@ -101,6 +66,21 @@ return {
     {
       "<Leader>tr",
       function()
+        local frequent_command = {
+          { "Show File Path", "call setqflist([{'text': execute('echomsg expand(\"%:p\")')}]) | copen" },
+          {
+            "Open Man In Floaterm",
+            "FloatermNew --width=0.8 --height=0.8 man -k . | fzf | awk '{print $1}' | xargs man | less",
+          },
+          {
+            "输出一个msg到quickfix",
+            "execute 'call setqflist([{\"text\": \"' . input('Enter message: ') . '\"}], \"a\") | copen'",
+          },
+          {
+            "Tokei(count code)",
+            "AsyncRun -mode=term -pos=floaterm -width=0.8 -height=0.8 tokei <root>",
+          },
+        }
         local picker = require("plugins.util.picker") -- lazy load
         picker.telescope_command_picker(frequent_command)
         -- picker.dress_select(frequent_command)
@@ -112,6 +92,29 @@ return {
       "<Leader>ty",
       function()
         -- picker.dress_select(yadm_command)
+        local yadm_command = {
+          {
+            "Yadm log",
+            -- "call asyncrun#run('', {'scroll':'0', 'post':'copen'}, 'cd ~ && yadm log')" ,
+            "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm log",
+          },
+          {
+            "Yadm nvim",
+            "call asyncrun#run('', {'cwd':'~'},'yadm add .config/nvim && yadm commit -m \"'.input('Enter commit message: ').'\" && yadm push')",
+          },
+          {
+            "Yadm diff",
+            "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm diff",
+          },
+          {
+            "Yadm status",
+            "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm status",
+          },
+          {
+            "Yadm diff this file",
+            "AsyncRun -cwd=~ -mode=term -pos=floaterm -width=0.8 -height=0.8 yadm diff $(VIM_FILEPATH)",
+          },
+        }
         local picker = require("plugins.util.picker") -- lazy load
         picker.telescope_command_picker(yadm_command)
       end,
@@ -121,6 +124,16 @@ return {
     {
       "<Leader>tg",
       function()
+        local git_command = {
+          {
+            "查看git仓库代码修改行数",
+            "AsyncRun -cwd=<root> -mode=term -pos=floaterm -width=0.8 -height=0.8 git diff --stat HEAD",
+          },
+          {
+            "Git This Repository",
+            "call asyncrun#run('', {'cwd':'<root>'},'git add . && git commit -m \"'.input('Enter commit message: ').'\" && git push')",
+          },
+        }
         local picker = require("plugins.util.picker") -- lazy load
         -- picker.dress_select(yadm_command)
         picker.telescope_command_picker(git_command)
